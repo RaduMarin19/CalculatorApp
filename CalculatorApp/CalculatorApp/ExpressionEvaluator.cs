db5f1dc139ca2ba88c883b65126baaae1aa67ad5
RaduMarin19 <marinradul33t@gmail.com>
2025-02-28 14:48:26 +0200

    -Added RPN Evaluator for making it easier to scale the app later

---
+++ CalculatorApp/CalculatorApp/ExpressionEvaluator.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace CalculatorApp
+{
+    public class ExpressionEvaluator
+    {
+        private static bool IsOperator(char c)
+        {
+            return c == '^' || c == '*' || c == '/' || c == '+' || c == '-';
+        }
+
+        private static bool IsValidCharacter(char c)
+        {
+            return char.IsDigit(c) || IsOperator(c) || c == '(' || c == ')' || c == '.';
+        }
+
+        public static bool ValidateExpression(string expression)
+        {
+            Stack<char> parenthesesStack = new Stack<char>();
+
+            foreach (char c in expression)
+            {
+                if (!IsValidCharacter(c))
+                    return false;
+
+                if (c == '(')
+                    parenthesesStack.Push(c);
+                else if (c == ')')
+                {
+                    if (parenthesesStack.Count == 0)
+                        return false;
+                    parenthesesStack.Pop();
+                }
+            }
+
+            return parenthesesStack.Count == 0;
+        }
+
+        private static int GetPrecedence(char c)
+        {
+            return c switch
+            {
+                '^' => 5,
+                '*' or '/' => 3,
+                '+' or '-' => 2,
+                '(' => 0,
+                _ => -1
+            };
+        }
+
+        public static List<string> ConvertToRPN(string expression)
+        {
+            List<string> rpn = new List<string>();
+            Stack<char> operatorStack = new Stack<char>();
+            string number = "";
+
+            foreach (char c in expression)
+            {
+                if (char.IsDigit(c) || c == '.' || c=='-' && number=="")
+                {
+                    number += c;
+                }
+                else if (IsValidCharacter(c))
+                {
+                    if (!string.IsNullOrEmpty(number))
+                    {
+                        rpn.Add(number);
+                        number = "";
+                    }
+
+                    if (c == ')')
+                    {
+                        while (operatorStack.Count > 0 && operatorStack.Peek() != '(')
+                        {
+                            rpn.Add(operatorStack.Pop().ToString());
+                        }
+                        if (operatorStack.Count > 0)
+                            operatorStack.Pop();
+                    }
+                    else
+                    {
+                        while (operatorStack.Count > 0 && GetPrecedence(c) <= GetPrecedence(operatorStack.Peek()))
+                        {
+                            rpn.Add(operatorStack.Pop().ToString());
+                        }
+                        operatorStack.Push(c);
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(number))
+            {
+                rpn.Add(number);
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                rpn.Add(operatorStack.Pop().ToString());
+            }
+
+            return rpn;
+        }
+
+        private static double ApplyOperator(double operand1, double operand2, char op)
+        {
+            return op switch
+            {
+                '-' => operand1 - operand2,
+                '+' => operand1 + operand2,
+                '*' => operand1 * operand2,
+                '/' => operand2 != 0 ? operand1 / operand2 : throw new DivideByZeroException("Division by zero"),
+                '^' => Math.Pow(operand1, operand2),
+                _ => 0
+            };
+        }
+
+        public static double EvaluateRPN(List<string> rpn)
+        {
+            Stack<double> stack = new Stack<double>();
+
+            foreach (string token in rpn)
+            {
+                if (double.TryParse(token, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                {
+                    stack.Push(number);
+                }
+                else if (IsOperator(token[0]) && stack.Count >= 2)
+                {
+                    double operand2 = stack.Pop();
+                    double operand1 = stack.Pop();
+                    stack.Push(ApplyOperator(operand1, operand2, token[0]));
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Invalid expression format. Unexpected token: {token}");
+                }
+            }
+
+            return stack.Peek();
+        }
+    }
+}
--- CalculatorApp/CalculatorApp/ViewModel.cs
+++ CalculatorApp/CalculatorApp/ViewModel.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Configuration;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CalculatorApp
 {
     public class ViewModel : INotifyPropertyChanged
     {
-        public ViewModel() {
-            m_model = new Model();
+        public ViewModel()
+        {
             m_display = "0";
             m_secondDisplay = string.Empty;
+            m_expression = string.Empty;
             m_isOperatorClick = false;
 
             NumberCommand = new RelayCommand(OnNumberClick);
@@ -26,19 +25,13 @@ namespace CalculatorApp
         public string DisplayText
         {
             get { return m_display; }
-            set { 
+            set
+            {
                 m_display = value;
                 OnPropertyChanged(nameof(DisplayText));
             }
         }
-        public string ClearOption
-        {
-            get { return m_clearOption; }
-            set {
-                m_clearOption = value;
-                OnPropertyChanged(nameof(ClearOption));
-            }
-        }
+
         public string SecondDisplayText
         {
             get { return m_secondDisplay; }
@@ -48,98 +41,112 @@ namespace CalculatorApp
                 OnPropertyChanged(nameof(SecondDisplayText));
             }
         }
-        public bool IsOperatorClick
-        {
-            get { return m_isOperatorClick; }
-            set { m_isOperatorClick = value;}
-        }
 
         private void OnNumberClick(object obj)
         {
-            if(m_isOperatorClick || DisplayText == "0") 
-            {   DisplayText = string.Empty;}
+            if (m_isOperatorClick || DisplayText == "0")
+            {
+                DisplayText = string.Empty;
+            }
 
             DisplayText += obj?.ToString();
+            m_expression += obj?.ToString();
             m_isOperatorClick = false;
-            if(m_operator==null)
-            { m_model.FirstNumber = double.Parse(DisplayText); }
-            else { m_model.SecondNumber = double.Parse(DisplayText); }
         }
+
         private void OnClearClick(object obj)
         {
-            m_clearOption = obj.ToString();
-            switch (m_clearOption)
+            string clearOption = obj.ToString();
+
+            switch (clearOption)
             {
                 case "C":
                     DisplayText = "0";
                     SecondDisplayText = string.Empty;
-                    m_model.FirstNumber = 0;
-                    m_model.SecondNumber = 0;
-                    m_operator = null;
+                    m_expression = string.Empty;
                     break;
                 case "CE":
                     DisplayText = "0";
-                    if(m_model.SecondNumber != 0)
+                    if (m_expression.Length > 0)
                     {
-                        m_model.SecondNumber = 0;
-                    }
-                    else
-                    {
-                        m_model.FirstNumber = 0;
-                        m_operator = null;
-                        SecondDisplayText= string.Empty;
+                        m_expression = string.Empty;
                     }
                     break;
                 case "R":
+                    if (DisplayText.Length > 1)
+                    {
+                        DisplayText = DisplayText.Substring(0, DisplayText.Length - 1);
+                        m_expression = m_expression.Substring(0, m_expression.Length - 1);
+                    }
+                    else
+                    {
+                        DisplayText = "0";
+                        m_expression = string.Empty;
+                    }
                     break;
                 default:
                     break;
             }
         }
+
         private void OnOperatorClick(object obj)
         {
-            m_operator = obj.ToString();
-            m_model.Operator = m_operator;
-            if (m_operator == "S") 
-            { SecondDisplayText = "sqrt(" + m_model.FirstNumber.ToString() + ")"; }
-            else 
-            { SecondDisplayText = m_model.FirstNumber.ToString() + m_operator; }
+            string op = obj.ToString();
+
+            if (m_expression.Length > 0 && "+-*/^".Contains(m_expression[^1]))
+            {
+                m_expression = m_expression.Substring(0, m_expression.Length) + op;
+                SecondDisplayText = m_expression;
+            }
+            else
+            {
+                if(m_expression.Any(c => "+-*/^".Contains(c)))
+                {
+                    OnCalculateClick(obj);
+                }
+                m_expression += op;
+                SecondDisplayText = m_expression;
+            }
+
             m_isOperatorClick = true;
         }
+
         private void OnCalculateClick(object obj)
         {
-            if(m_operator == null)
-            { return; }
-            if (m_operator == "^")
-            { SecondDisplayText += "2="; }
-            else if(m_operator == "S")
-            { SecondDisplayText += "="; }
-            else 
-            { SecondDisplayText += m_model.SecondNumber.ToString() + '='; }
-            DisplayText = m_model.Calculate().ToString();
-            m_model.FirstNumber=double.Parse(DisplayText);
-            m_model.SecondNumber = 0;
-            m_operator = null;
-        }
+            if (string.IsNullOrEmpty(m_expression))
+                return;
+            
+            try
+            {
+                List<string> rpn = ExpressionEvaluator.ConvertToRPN(m_expression);
+                double result = ExpressionEvaluator.EvaluateRPN(rpn);
 
+                SecondDisplayText += DisplayText + " =";
+                DisplayText = result.ToString();
+
+                m_expression = result.ToString(); 
+            }
+            catch (Exception ex)
+            {
+                DisplayText = "Error";
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
         public ICommand NumberCommand { get; set; }
         public ICommand OperatorCommand { get; set; }
         public ICommand ClearCommand { get; set; }
         public ICommand CalculateCommand { get; set; }
 
-        private Model m_model;
-        private String m_display;
-        private String m_operator;
-        private String m_secondDisplay;
-        private String m_clearOption;
+        private string m_display;
+        private string m_secondDisplay;
+        private string m_expression;
         private bool m_isOperatorClick;
-
-
     }
 }
