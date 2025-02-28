7a23c3e8bf8ce0240076b98c9aebb0c4e2a12d9a
RaduMarin19 <marinradul33t@gmail.com>
2025-02-28 11:10:40 +0200

    Added project files and buttons for standard controls

---
+++ CalculatorApp/CalculatorApp/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Model
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+
+        public String Operator { get; set; }
+
+        public double Calculate()
+        {
+            return Operator switch
+            {
+                "+" => FirstNumber + SecondNumber,
+                "-" => FirstNumber - SecondNumber,
+                "*" => FirstNumber * SecondNumber,
+                "/" => SecondNumber!=0 ? FirstNumber * SecondNumber : 0,
+                _ => 0,
+            };
+        }
+
+    }
+}
